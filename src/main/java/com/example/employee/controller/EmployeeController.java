b8c71b00b4bb0678cd107458f34104d5f30e31c2
hoangviet27785 <hoangviet27785@gmail.com>
2025-06-13 15:34:24 +0700

    update source

--- src/main/java/com/example/employee/controller/EmployeeController.java
+++ src/main/java/com/example/employee/controller/EmployeeController.java
@@ -1,23 +1,22 @@
 package com.example.employee.controller;
 
 import com.example.employee.model.Employee;
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import org.springframework.core.io.ClassPathResource;
+import com.example.employee.service.EmployeeService;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.ui.Model;
 
-import java.io.InputStream;
 import java.util.List;
 
 @RestController
 public class EmployeeController {
+    @Autowired
+    private EmployeeService employeeService;
 
     @GetMapping("/employees")
     public String getEmployees(Model model) throws Exception {
-        ObjectMapper mapper = new ObjectMapper();
-        InputStream inputStream = new ClassPathResource("employees.json").getInputStream();
-        List<Employee> employees = mapper.readValue(inputStream, new TypeReference<List<Employee>>() {});
+        List<Employee> employees = employeeService.getAllEmployees();
         model.addAttribute("employees", employees);
         return "employees"; // tên file HTML trong thư mục templates (employees.html)
     }
